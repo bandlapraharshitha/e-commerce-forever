825575f1a45c56948aebc71a54fffbe92f230093
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-23 11:28:22 +0530

    Fixed frontend folder issue

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,600 @@
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+import cart_icon from './cart_icon.png'
+import menu_icon from './menu_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import cross_icon from './cross_icon.png'
+import hero_img from './hero_img.png'
+import exchange_icon from './exchange_icon.png'
+import quality_icon from './quality_icon.png'
+import support_img from './support_img.png'
+import p_img1 from './p_img1.png'
+import p_img2 from './p_img2.png'
+import p_img3 from './p_img3.png'
+import p_img4 from './p_img4.png'
+import p_img5 from './p_img5.png'
+import p_img6 from './p_img6.png'
+import p_img7 from './p_img7.png'
+import p_img8 from './p_img8.png'
+import p_img9 from './p_img9.png'
+import p_img10 from './p_img10.png'
+import p_img11 from './p_img11.png'
+import p_img12 from './p_img12.png'
+import p_img13 from './p_img13.png'
+import p_img14 from './p_img14.png'
+import p_img15 from './p_img15.png'
+import p_img16 from './p_img16.png'
+import p_img17 from './p_img17.png'
+import p_img18 from './p_img18.png'
+import p_img19 from './p_img19.png'
+import p_img20 from './p_img20.png'
+import p_img21 from './p_img21.png'
+import p_img22 from './p_img22.png'
+import p_img23 from './p_img23.png'
+import p_img24 from './p_img24.png'
+import p_img25 from './p_img25.png'
+import p_img26 from './p_img26.png'
+import p_img27 from './p_img27.png'
+import p_img28 from './p_img28.png'
+import p_img29 from './p_img29.png'
+import p_img30 from './p_img30.png'
+import p_img31 from './p_img31.png'
+import p_img32 from './p_img32.png'
+import p_img33 from './p_img33.png'
+import p_img34 from './p_img34.png'
+import p_img35 from './p_img35.png'
+import p_img36 from './p_img36.png'
+import p_img37 from './p_img37.png'
+import p_img38 from './p_img38.png'
+import p_img39 from './p_img39.png'
+import p_img40 from './p_img40.png'
+import p_img41 from './p_img41.png'
+import p_img42 from './p_img42.png'
+import p_img43 from './p_img43.png'
+import p_img44 from './p_img44.png'
+
+export const assets = {
+    logo,
+    search_icon,
+    profile_icon,
+    cart_icon,
+    menu_icon,
+    dropdown_icon,
+    cross_icon,
+    hero_img,
+    exchange_icon,
+    quality_icon,
+    support_img
+}
+
+export const products = [
+    {
+        _id: "aaaa",
+        name: "Women cotton casual bottom wear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 100,
+        image: [p_img1],
+        category: "Women",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaab",
+        name: "Men Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 80,
+        image: [p_img2],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaac",
+        name: "Kids men coat Topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 60,
+        image: [p_img3],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaad",
+        name: "Women Round Neck casual Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 40,
+        image: [p_img4],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaae",
+        name: "Men bottomwear cotton pant",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 30,
+        image: [p_img5],
+        category: "Men",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaf",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 35,
+        image: [p_img6],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaag",
+        name: "Women winterwear light",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 45,
+        image: [p_img7],
+        category: "Women",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaah",
+        name: "Kids cotton bottomwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 50,
+        image: [p_img8],
+        category: "Kids",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaai",
+        name: "Men Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 70,
+        image: [p_img9],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaj",
+        name: "Women Round Neck winter topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 90,
+        image: [p_img10],
+        category: "Women",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaak",
+        name: "Men winterwear light",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 110,
+        image: [p_img11],
+        category: "Men",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaal",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 130,
+        image: [p_img12],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaam",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 150,
+        image: [p_img13],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaan",
+        name: "Men winterwear casual",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 140,
+        image: [p_img14],
+        category: "Men",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaao",
+        name: "Men winterwear simple",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 120,
+        image: [p_img15],
+        category: "Men",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaap",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 95,
+        image: [p_img16],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaq",
+        name: "Kids Round Neck Cotton topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 85,
+        image: [p_img17],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaar",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 75,
+        image: [p_img18],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaas",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 65,
+        image: [p_img19],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaat",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 105,
+        image: [p_img20],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaau",
+        name: "Women Round Neck winter topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 115,
+        image: [p_img21],
+        category: "Women",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaav",
+        name: "Men bottomwear cotton pant",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 125,
+        image: [p_img22],
+        category: "Men",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaw",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 135,
+        image: [p_img23],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaax",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 145,
+        image: [p_img24],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaay",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 155,
+        image: [p_img25],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaz",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 57,
+        image: [p_img26],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaba",
+        name: "Kids cotton bottomwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 62,
+        image: [p_img27],
+        category: "Kids",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabb",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 78,
+        image: [p_img28],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabc",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 84,
+        image: [p_img29],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabd",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 92,
+        image: [p_img30],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabe",
+        name: "Women cotton simple bottomwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 61,
+        image: [p_img31],
+        category: "Women",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabf",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 103,
+        image: [p_img32],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabg",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 144,
+        image: [p_img33],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabh",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 69,
+        image: [p_img34],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabi",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 137,
+        image: [p_img35],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabj",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 170,
+        image: [p_img36],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabk",
+        name: "Women cotton simple bottomwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 89,
+        image: [p_img37],
+        category: "Women",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabl",
+        name: "Men simple top winterwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 119,
+        image: [p_img38],
+        category: "Men",
+        subCategory: "Winterwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabm",
+        name: "Kids party topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 137,
+        image: [p_img39],
+        category: "Kids",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabn",
+        name: "Women cotton simple bottomwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 151,
+        image: [p_img40],
+        category: "Women",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabo",
+        name: "Men bottomwear cotton pant",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 123,
+        image: [p_img41],
+        category: "Men",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabp",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 99,
+        image: [p_img42],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aabq",
+        name: "Men bottomwear cotton pant",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 101,
+        image: [p_img43],
+        category: "Men",
+        subCategory: "Bottomwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aabr",
+        name: "Men round neck topwear",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with zip",
+        price: 149,
+        image: [p_img44],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+]
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import BestSeller from "../components/BestSeller"
+import Hero from "../components/Hero"
+import LatestCollection from "../components/LatestCollection"
+import NewsletterBox from "../components/NewsletterBox"
+import OurPolicy from "../components/OurPolicy"
+
+
+const home = () => {
+    return (
+      <div>
+        <Hero />
+        <LatestCollection />
+        <BestSeller />
+        <OurPolicy />
+        <NewsletterBox />
+      </div>
+    )
+  }
+  
+  export default home
