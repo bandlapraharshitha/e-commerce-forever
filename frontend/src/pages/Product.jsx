8b1cd26cc105aefb50a5966fdbbd2a4d6c836dc9
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-23 23:23:33 +0530

    feat:Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,27 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import { ShopContext } from './../context/ShopContext';
 
+const Product = () => {
 
-const cart = () => {
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+    products.map((item)=>{
+      if (item._id === productId) {
+        setProductData(item)
+        console.log(item);
+        return null;
+      }
+    })
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId])
+
     return (
       <div>
         
@@ -8,4 +29,4 @@ const cart = () => {
     )
   }
   
-  export default cart
+  export default Product
