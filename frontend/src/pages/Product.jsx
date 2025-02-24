f31d7aa51eb8be8a817f46e86921e8f724e5f1cb
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-24 11:24:50 +0530

    feat:RelatedProducts.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { ShopContext } from './../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from './../components/RelatedProducts';
 
 const Product = () => {
 
@@ -87,7 +88,7 @@ const Product = () => {
         </div>
 
         {/* -------------------- Display related products ----------------------- */}
-        
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
 
       </div>
     ) : <div className='opacity-0'></div>
