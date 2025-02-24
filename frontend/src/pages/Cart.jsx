19e9aded95910f1f32746df46c06d2918edb3e65
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-25 01:28:07 +0530

    feat:cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -47,7 +47,7 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input type="number" min={1} defaultValue={item.quantity} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" />
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} type="number" min={1} defaultValue={item.quantity} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" />
                 <img onClick={()=>updateQuantity(item._id,item.size,0)} src={assets.bin_icon} className="w-4 mr-4 sm:w-5 cursor-pointer" alt="" />
               </div>
             )
