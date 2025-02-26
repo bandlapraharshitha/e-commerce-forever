90c35cd461beec5de27d80aa91151a87f44e2ad8
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-26 18:18:26 +0530

    full About.jsx page

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -14,6 +14,7 @@ import star_dull_icon from './star_dull_icon.png'
 import bin_icon from './bin_icon.png'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
+import about_img from './about_img.png'
 import p_img1 from './p_img1.png'
 import p_img2 from './p_img2.png'
 import p_img3 from './p_img3.png'
@@ -78,7 +79,8 @@ export const assets = {
     star_dull_icon,
     bin_icon,
     stripe_logo,
-    razorpay_logo
+    razorpay_logo,
+    about_img
 }
 
 export const products = [
