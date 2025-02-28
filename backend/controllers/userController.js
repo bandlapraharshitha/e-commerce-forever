c992c0b9ce6d85df15a08aa05cee80b23fdb069e
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-28 20:16:22 +0530

    feat:userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// Route for user login
+const loginUser = async (req,res) => {
+
+}
+
+// Route for user register
+const registerUser = async (req,res) => {
+
+}
+
+// Route for admin login
+const adminLogin = async () => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
