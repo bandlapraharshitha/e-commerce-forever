aeff0a3bb8ae650924a59bbb991f90cfc24660c5
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-04 23:56:42 +0530

    singleProduct in productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,3 +14,8 @@ const listProducts = async (req, res) => {
 const removeProduct = async (req, res) => {
     
 }
+
+// function for single product info
+const singleProduct = async (req, res) => {
+    
+}
