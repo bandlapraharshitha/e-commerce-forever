7fe669877d80d742e6c2611aa1254032a38cb6bf
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-05 23:53:52 +0530

    post requests in productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,2 +1,9 @@
 import express from "express"
 import { listProducts, addProduct, removeProduct, singleProduct } from "../controllers/productController"
+
+const productRouter = express.Router();
+
+productRouter.post('/add',addProduct);
+productRouter.post('/remove',removeProduct);
+productRouter.post('/single',singleProduct);
+productRouter.get('/add',listProducts);
