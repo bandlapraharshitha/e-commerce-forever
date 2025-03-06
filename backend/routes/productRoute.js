821cf44cc4efc6c9c9818ffdbdf2f39f225b6b65
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-06 23:56:09 +0530

    importing multer in productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { listProducts, addProduct, removeProduct, singleProduct } from "../controllers/productController"
+import upload from "../middleware/multer";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',addProduct);
+productRouter.post('/add',upload.fields(),addProduct);
 productRouter.post('/remove',removeProduct);
 productRouter.post('/single',singleProduct);
 productRouter.get('/add',listProducts);
