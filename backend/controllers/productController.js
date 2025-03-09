00d4c7e21713f9dffae1eb634b2da6cbfe4d11eb
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-09 23:37:00 +0530

    image4 in addProduct in productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,7 @@ const addProduct = async (req, res) => {
         const image1 = req.files.image1[0]
         const image2 = req.files.image2[0]
         const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
     } catch (error) {
         
     }
