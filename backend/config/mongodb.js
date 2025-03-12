5be2efbbdab265478c4c4e919b787d85991ab540
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-12 23:34:03 +0530

    minute changes in mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-     mongoose.connection.on('connected',()=>{
+     mongoose.connection.on('connected', ()=>{
         console.log("DB Connected");
      })
      await mongoose.connect(`${process.env.MONGODB_URI}/Forever(e-com)`)
