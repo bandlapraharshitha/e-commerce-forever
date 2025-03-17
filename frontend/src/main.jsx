417214906d3d8e94c22ad92dd602da7038dae95a
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-17 23:42:05 +0530

    edit main.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@
 import  ReactDOM  from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
